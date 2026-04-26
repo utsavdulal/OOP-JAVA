11579b4e8f230750571c28e1e0520d8b77948449
Utsav Dulal <utsavdulal6@gmail.com>
2026-04-26 11:33:59 +0545

    Delete Desktop directory

--- Desktop/school/client/src/components/admin/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="flex justify-center items-center py-8">
-      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
-    </div>
-  );
-};
-
-export default LoadingSpinner;
--- Desktop/school/client/src/components/admin/Toast.jsx
+++
@@ -1,7 +0,0 @@
-import { toast } from 'react-hot-toast';
-
-export const showToast = {
-  success: (message) => toast.success(message, { duration: 3000 }),
-  error: (message) => toast.error(message, { duration: 3000 }),
-  loading: (message) => toast.loading(message)
-};
